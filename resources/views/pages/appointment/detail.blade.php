6c274c6890f743bff5e1798e81a8bf20b1b93573
FaizRahman01 <mdfaiz.rahman@gmail.com>
2024-05-22 17:22:03 +0800

    abc

--- resources/views/pages/appointment/detail.blade.php
+++ resources/views/pages/appointment/detail.blade.php
@@ -110,16 +110,6 @@
                                 <div class="accordion-body">
                                     <form action="#" method="post">
                                         <div class="mb-3">
-                                            <label for="name" class="form-label">Name</label>
-                                            <input type="text" class="form-control" id="name" name="name"
-                                                required>
-                                        </div>
-                                        <div class="mb-3">
-                                            <label for="email" class="form-label">Email</label>
-                                            <input type="email" class="form-control" id="email" name="email"
-                                                required>
-                                        </div>
-                                        <div class="mb-3">
                                             <label for="feedback" class="form-label">Feedback</label>
                                             <textarea class="form-control" id="feedback" name="feedback" required></textarea>
                                         </div>
