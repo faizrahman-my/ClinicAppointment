21ac9a2e38084e828455b3bec0fa66ff3bd528aa
FaizRahman01 <mdfaiz.rahman@gmail.com>
2024-05-22 22:45:52 +0800

    add and change column name

--- database/migrations/2024_05_22_090007_create_appointments_table.php
+++ database/migrations/2024_05_22_090007_create_appointments_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('appointments', function (Blueprint $table) {
             $table->id();
             $table->date('reservation_date');
+            $table->string('reservation_status');
             $table->string('status');
             $table->mediumText('doctor_desc')->nullable();
             $table->mediumText('doctor_comment')->nullable();
